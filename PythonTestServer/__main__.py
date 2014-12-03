c251b60c33f5de0ef6e7ce348c81945fb8b831a3
Larissa Laich <la8895@web.de>
2014-12-04 00:05:24 +0100

    Test for GET and PUT Method added, made same requests

--- PythonTestServer/__main__.py
+++ PythonTestServer/__main__.py
@@ -18,5 +18,25 @@ def test_delete():
 	print('delete')
 	return jsonify({"deletion": 'successful'})
 
+@app.route('/test/get', methods=['GET'])
+def test_get():
+	print('get')
+	return jsonify({"get": 'successful'})
+
+@app.route('/test/put/<string(length=2):code>', methods=['PUT'])
+def test_put(code):
+	#args: A MultiDict with the parsed contents of the query string. (The part in the URL after the question mark).
+	#e.g: http://127.0.0.1:5000/test/put/st?number=23
+	number = request.args.get('number', '3,1415')
+	#in the request header (not on body)
+	test = request.headers.get('test')
+	print('put')
+	print(number)
+	print(test)
+	print(code)
+	return jsonify({"put": 'successful'})
+
+
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
