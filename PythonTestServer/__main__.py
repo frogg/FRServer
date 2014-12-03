6de8d2665e35d50fb33db3aa30f6cab2665c06d9
Larissa Laich <la8895@web.de>
2014-12-03 23:16:47 +0100

    server is working, post method add

--- PythonTestServer/__main__.py
+++ PythonTestServer/__main__.py
@@ -1,1 +1,17 @@
 #python test server
+from flask import Flask, jsonify,request
+
+app = Flask(__name__)
+
+@app.route('/test/post', methods=['POST'])
+def test_post():
+	name = request.form.get('name',None)
+	print(name)
+	#default value
+	lastName = request.form.get('lastName',"Quappi")
+	print(lastName)
+	toReturn = {"success": True, "Hi": lastName}
+	return jsonify(toReturn);
+
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0')
