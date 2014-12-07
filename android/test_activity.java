8b72b96f9f7ece64254acf4f985d111fdeb87737
Larissa Laich <la8895@web.de>
2014-12-07 10:59:10 +0100

    android test loading added

---
+++ android/test_activity.java
@@ -0,0 +1,52 @@
+package project.server;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.json.JSONArray;
+
+
+public class test_activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_activity);
+
+            JSONParser json = new JSONParser() {
+            @Override
+            public void onJSONLoaded(JSONArray jArr) {
+            Log.d("LogsLar","JSONLoaded");
+            }
+
+            @Override
+            public void JSONNotLoaded() {
+            Log.d("LogsLar","JSONNotLoaded");
+            }
+        };
+        json.execute("http://www.kantinele.de/api/menus/getnextxdays/14");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.test_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
